d5c6fc2484f02d0139d4e380277102d787290d4a
Levi Schneider <levi.schneider2010@gmail.com>
2019-07-13 08:50:36 -0400

    Adding a tilemap and player animations

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -12,14 +12,28 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x=SPEED
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Run", false)
 	elif Input.is_action_pressed("ui_left"):
 		motion.x=-SPEED
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Run", false)
 	else:
 		motion.x = 0
+		$AnimatedSprite.play("Idle", false)
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y=JUMP_HEIGHT
+	else:
+		$AnimatedSprite.play("Jump", false)
 			
 	motion = move_and_slide(motion, UP)
 	pass
+	
+	
+	
+	
+	
+	
+	
