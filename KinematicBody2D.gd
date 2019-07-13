8c101683716a439bcdbe4ddedaef94712a893f2a
Levi Schneider <levi.schneider2010@gmail.com>
2019-07-13 09:32:55 -0400

    Adding upgrading the movement physics and collision boundaries

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,31 +2,40 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -550
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x=SPEED
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("Run", false)
 	elif Input.is_action_pressed("ui_left"):
-		motion.x=-SPEED
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("Run", false)
 	else:
-		motion.x = 0
+		friction = true
 		$AnimatedSprite.play("Idle", false)
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y=JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$AnimatedSprite.play("Jump", false)
+		if motion.y < 0:
+			$AnimatedSprite.play("Jump", false)
+		else:
+			$AnimatedSprite.play("Fall", false)
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 			
 	motion = move_and_slide(motion, UP)
 	pass
